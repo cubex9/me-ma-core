d0435d10093324e8b4d6089145dc5e67e1aace8b
cubex9 <cube.x.nine@gmail.com>
2016-07-02 11:38:09 +0200

    jersy -> restfull

---
+++ src/main/java/eu/cxn/mema/rest/MemaApi.java
@@ -0,0 +1,23 @@
+package eu.cxn.mema.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+
+/**
+ * Created by kubasek on 7/2/2016.
+ */
+
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/")
+public class MemaApi {
+
+    @GET
+    @Produces("text/plain")
+    public String api() {
+        // Return some cliched textual content
+        return "This is me-ma-core rest service";
+    }
+}
+
---
+++ src/main/java/eu/cxn/mema/rest/NodesRs.java
@@ -0,0 +1,9 @@
+package eu.cxn.mema.rest;
+
+/**
+ * Created by kubasek on 7/2/2016.
+ */
+public interface NodesRs {
+
+
+}
