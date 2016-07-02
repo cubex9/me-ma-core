400dc220bef0eb87328a3c114dbc7ac639e9ed64
cubex9 <cube.x.nine@gmail.com>
2016-07-02 22:27:57 +0200

    jersy -> restfull2 ok

--- src/main/java/eu/cxn/mema/rest/NodesRs.java
+++ src/main/java/eu/cxn/mema/rest/NodesRs.java
@@ -1,5 +1,7 @@
 package eu.cxn.mema.rest;
 
+import eu.cxn.mema.StaticStore;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,12 +10,17 @@ import javax.ws.rs.Produces;
  * Created by kubasek on 7/2/2016.
  */
 @Path("/nodes")
+@Produces("text/plain")
 public class NodesRs {
 
     @GET
-    @Produces("text/plain")
-    public String getList() {
-        return "AHOJ";
+    @Path("/info")
+    public String info() {
+        return StaticStore.net.toString();
     }
 
+    @GET
+    public String getList() {
+        return "nic";
+    }
 }
